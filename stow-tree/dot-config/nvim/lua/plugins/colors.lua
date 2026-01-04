bb4adcc641b9ae9a8e94bc734805f81578d121f4
Aspen Price <aspen@aspen-otr.com>
2026-01-04 02:08:02 -0600

    final commit, RIP

--- stow-tree/dot-config/nvim/lua/plugins/colors.lua
+++ stow-tree/dot-config/nvim/lua/plugins/colors.lua
@@ -1,12 +1,21 @@
 return {
     {
-        "Shatur/neovim-ayu",
+        "morhetz/gruvbox",
         lazy = false,
         priority = 1000,
         config = function()
             vim.opt.background = "dark"
-            require("ayu").setup { mirage = true }
-            vim.cmd [[ colorscheme ayu ]]
+            vim.cmd [[ colorscheme gruvbox ]]
         end
+    },
+    {
+        "Shatur/neovim-ayu",
+        lazy = false,
+        priority = 1000,
+        -- config = function()
+        --     vim.opt.background = "dark"
+        --     require("ayu").setup { mirage = true }
+        --     vim.cmd [[ colorscheme ayu ]]
+        -- end
     }
 }
